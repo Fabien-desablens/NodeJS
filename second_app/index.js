3cd46add267c9907266e3c0b0d3b90213f963df6
Fabien-desablens <fabienbecode@gmail.com>
2019-07-09 10:07:22 +0200

    firstcommit

--- second_app/index.js
+++ second_app/index.js
@@ -1,6 +1,7 @@
 // import the NPM dependancy package
 const express     = require("express");
 const bodyParser  = require("body-parser");
+const mongoose    = require("mongoose");
 
 // initialise express() inside of your app variable
 const app = express();
@@ -8,8 +9,11 @@ const app = express();
 // parse body of incoming json requests
 app.use(bodyParser.json());
 
-// import route modules and pass your app
-require("./routes/userRoutes")(app);
+// import route- and model modules and pass your app
+require("./models/Pet")(app);
+require("./routes/petRoutes")(app);
+
+mongoose.connect("mongodb://username:123password@ds251902.mlab.com:51902/testerino");
 
 // choose what port on which to run the server
 const PORT = 5000;
